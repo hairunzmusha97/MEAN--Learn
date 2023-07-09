fadaeff04c374734760e846de5ab02925875f6a2
Mohamed Musharrif <musharrif@spil.com.au>
2023-07-09 20:48:32 +0530

    home component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/partials/header/header.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { StarRatingComponent } from './component/partials/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
